7a03b9f47ce5b06a7f39f259b1c51d63e66b2704
kbrzdem <kbr_ozdm@hotmail.com>
2021-07-11 20:24:41 +0300

    created

--- src/components/appletree.js
+++ src/components/appletree.js
@@ -4,7 +4,6 @@ const Apple = () => {
   const [click, setClick] = useState(false);
   const [isClass, setClass] = useState(false);
   const [isBasket, setBasket] = useState(false);
-
   //const random = Math.floor(Math.random() * 3) + 1;
   const toggleClass = () => {
     setClick(!click);
